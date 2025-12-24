cb8b52e1c2baaebd0db8cbec0de8de653cfd24be
Sssss-Mmm <elation4927@gmail.com>
2025-12-24 22:22:17 +0900

    feat: DB 연결 URL을 환경 변수로 설정하고 Docker Compose에 MySQL DB 서비스를 추가

--- api/db.py
+++ api/db.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker , declarative_base
 
-DB_URL = "mysql+pymysql://root:root@localhost:3306/todo"
+import os
+
+# Docker 내부에서는 'db:3306', 로컬 직접 실행 시에는 'localhost:3307' 사용
+DB_URL = os.getenv("DB_URL", "mysql+pymysql://root:root@localhost:3307/todo")
 
 db_engine = create_engine(DB_URL)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
